03bf5424d7b86707b85e9ee8c4b9858e66b5a7ed
lcosta.engqua <lcosta.engqua@gmail.com>
2025-05-20 22:23:39 -0300

    navegação para outro arquivo

---
+++ exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets\css\estilo.css">
+
+        <title>Ecercício</title>
+    </head>
+    <body>
+        <header class="cabecalho">
+            <h1>Curso PHP</h1>
+            <h2>Visualização do Exercício</h2>
+        </header>
+        <main class="principal">
+            <div class="conteudo">
+                <nav class="modulos">
+                </nav>
+            </div>
+        </main>
+        <footer class="rodape">
+            COD3R & ALUNOS © <?= date('Y'); ?>
+        </footer>
+    </body>
+</html>
